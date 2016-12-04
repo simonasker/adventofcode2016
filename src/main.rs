0e38217d2b039d2b8cc74d13ae33e14559b4710c
Simon Andersson <simon@mullvad.net>
2016-12-04 19:49:02 +0100

    Setup clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+
+use clap::{Arg,App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Advent of code 2016")
+        .version("0.1")
+        .author("Simon Andersson <simonasker@posteo.net>")
+        .about("Solutions to Advent of code 2016")
+        .arg(Arg::with_name("day")
+             .short("d")
+             .long("day")
+             .value_name("DAY")
+             .help("The day to run")
+             .required(true)
+             .takes_value(true))
+        .get_matches();
+
+
+    let day = matches.value_of("day").unwrap();
+
+    println!("Running solution for day: {}", day);
 }
