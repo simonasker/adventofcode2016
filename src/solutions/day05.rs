64ab70efe0625adf4b79d3cf84e4d3c26ed9e7f0
Simon Andersson <simon@mullvad.net>
2016-12-05 21:23:27 +0100

    Ugly solution to day 5 part 2

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -44,5 +44,46 @@ fn part_one() {
 }
 
 fn part_two() {
-    println!("Not yet implemented");
+    let mut f = File::open("input/day05.txt").unwrap();
+    let mut input = String::new();
+    f.read_to_string(&mut input).unwrap();
+    let input = input.trim().to_owned();
+
+    let mut answer = ['-'; 8];
+    let mut found = [false; 8];
+
+    for n in 0.. {
+        let mut hasher = Md5::new();
+        let foo = format!("{}{}", input, n);
+        hasher.input_str(&foo);
+        let hash = hasher.result_str();
+
+        if hash.starts_with("00000") {
+            let position = hash.chars().nth(5).unwrap();
+            let character = hash.chars().nth(6).unwrap();
+
+            if let Some(pos) = position.to_digit(10) {
+                if pos < 8 && !found[pos as usize] {
+                    println!("{}: {}", foo, hash);
+                    answer[pos as usize] = character;
+                    found[pos as usize] = true;
+                    println!("{:?}", answer);
+                }
+            }
+        }
+
+        let mut done = true;
+
+        for b in found.iter() {
+            if !b {
+                done = false;
+            }
+        }
+
+        if done {
+            break;
+        }
+    }
+
+    println!("Answer: {:?}", answer);
 }
