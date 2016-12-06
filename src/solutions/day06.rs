410f54ab821dd7f258fc97485138213b5301d0a5
Simon Andersson <simon@mullvad.net>
2016-12-06 18:24:49 +0100

    Day 6 part 2 solved

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -44,5 +44,34 @@ fn part_one() {
 }
 
 fn part_two() {
-    println!("Not yet implemented");
+    let f = File::open("input/day06.txt").unwrap();
+    let reader = BufReader::new(f);
+
+    let mut maps: Vec<HashMap<char, u32>> = Vec::new();
+
+    for _ in 0..8 {
+        maps.push(HashMap::new());
+    }
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        for (i, c) in line.chars().enumerate() {
+            let ref mut letters = maps[i];
+            let counter = letters.entry(c).or_insert(0);
+            *counter += 1;
+        }
+    }
+
+    let mut answer = String::new();
+
+    for map in maps {
+        let mut chars: Vec<char> = map.keys().map(char::to_owned).collect();
+        chars.sort_by_key(|c| map.get(c).unwrap());
+        // Removing this is the only part that is different from part_one
+        // chars.reverse();
+        answer.push(chars[0]);
+    }
+
+    println!("Answer: {}", answer);
 }
