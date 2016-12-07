552b681513c1969025be566e4f0f876b35a0539c
Simon Andersson <simon@mullvad.net>
2016-12-07 18:13:13 +0100

    Create AbbaQueue type

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -10,6 +10,30 @@ pub fn run(part: i32) {
     }
 }
 
+#[derive(Debug)]
+struct AbbaQueue {
+    queue: [char; 4],
+}
+
+impl AbbaQueue {
+    fn new() -> Self {
+        AbbaQueue {
+            queue: ['-'; 4],
+        }
+    }
+
+    fn add(&mut self, c: char) {
+        self.queue[0] = self.queue[1];
+        self.queue[1] = self.queue[2];
+        self.queue[2] = self.queue[3];
+        self.queue[3] = c;
+    }
+
+    fn is_abba(&self) -> bool {
+        self.queue[0] == self.queue[3] && self.queue[1] == self.queue[2]
+    }
+}
+
 fn part_one() {
     let f = File::open("input/day07.txt").unwrap();
     let reader = BufReader::new(f);
@@ -18,6 +42,25 @@ fn part_one() {
         let line = line.unwrap();
         println!("{}", line);
 
+        let mut chars = line.chars();
+        let mut inside = false;
+        loop {
+            if let Some(c) = chars.next() {
+                match c {
+                    '[' => inside = true,
+                    ']' => inside = false,
+                    _ => {
+                        if inside {
+                            println!("IN  : {}", c);
+                        } else {
+                            println!("OUT : {}", c);
+                        }
+                    },
+                }
+            } else {
+                break;
+            }
+        }
     }
 }
 
