16f7a38da34848c1e1f3d3cd2dfaf3ffc4ba4482
Simon Andersson <simon@mullvad.net>
2016-12-08 20:13:31 +0100

    Create Screen type

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+const SCREEN_WIDTH: usize = 7;
+const SCREEN_HEIGHT: usize = 3;
+
 pub fn run(part: i32) {
     match part {
         1 => part_one(),
@@ -10,10 +13,47 @@ pub fn run(part: i32) {
     }
 }
 
+#[derive(Debug)]
+struct Screen {
+    array: [[u8; SCREEN_WIDTH]; SCREEN_HEIGHT],
+}
+
+impl Screen {
+    fn new() -> Self {
+        Screen {
+            array: [[0; SCREEN_WIDTH]; SCREEN_HEIGHT],
+        }
+    }
+
+    fn turn_on(&mut self, x: usize, y: usize) {
+        self.array[y][x] = 1;
+    }
+
+    fn count_pixels(&self) -> u32 {
+        let mut total: u32 = 0;
+
+        // TODO This could probably be done nicer
+        for a in self.array.iter() {
+            let sum: u8 = a.iter().sum();
+            total += sum as u32;
+        }
+
+        total
+    }
+}
+
 
 fn part_one() {
     let f = File::open("input/day08.txt").unwrap();
     let reader = BufReader::new(f);
+
+    let mut screen = Screen::new();
+    screen.turn_on(0, 0);
+    screen.turn_on(1, 1);
+    screen.turn_on(2, 2);
+    screen.turn_on(2, 2);
+    println!("{:?}", screen);
+    println!("Sum: {}", screen.count_pixels());
 
     for line in reader.lines() {
         let line = line.unwrap();
