a35801e31f336ff0fbba42dc1a60795b644b0d1b
Simon Andersson <simon@mullvad.net>
2016-12-09 15:53:15 +0100

    Initial commit of day 9

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -6,6 +6,7 @@ mod day05;
 mod day06;
 mod day07;
 mod day08;
+mod day09;
 
 pub fn run(day: i32, part: i32) {
     match day {
@@ -17,6 +18,7 @@ pub fn run(day: i32, part: i32) {
         6 => day06::run(part),
         7 => day07::run(part),
         8 => day08::run(part),
+        9 => day09::run(part),
         d @ _ => println!("Day {} is not yet implemented", d),
     }
 }
