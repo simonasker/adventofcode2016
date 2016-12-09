896ec24e3fd7354d1d61fe49e544e1a2f3ce85b3
Simon Andersson <simon@mullvad.net>
2016-12-09 17:01:17 +0100

    Day 9 part 2 solved

--- src/solutions/day09.rs
+++ src/solutions/day09.rs
@@ -55,6 +55,55 @@ fn part_one() {
 
 }
 
+fn split_marker(marker_str: String) -> (u32, u32) {
+    let mut split = marker_str.split('x');
+    let num_chars = u32::from_str(split.next().unwrap()).unwrap();
+    let reps = u32::from_str(split.next().unwrap()).unwrap();
+    (num_chars, reps)
+}
+
+fn unwind(input: &str) -> u64 {
+    println!("Unwinding: {}", input);
+    let mut result = 0u64;
+    let mut char_iter = input.chars();
+    loop {
+        let c = char_iter.next();
+        match c {
+            Some('(') => {
+                let marker: String = char_iter
+                    .by_ref()
+                    .take_while(|&c| c != ')')
+                    .collect();
+                let (num_chars, reps) = split_marker(marker);
+                let chars: String = char_iter
+                    .by_ref()
+                    .take(num_chars as usize)
+                    .collect();
+
+                let unwinded = unwind(chars.as_ref());
+                result += reps as u64 * unwinded;
+            },
+            Some(_) => {
+                result += 1;
+            },
+            None => break,
+        }
+    }
+
+    println!("Returning: {}", result);
+    result
+}
+
+
 fn part_two() {
-    println!("Not yet implemented");
+    let mut f = File::open("input/day09.txt").unwrap();
+
+    let mut input = String::new();
+
+    f.read_to_string(&mut input).unwrap();
+
+    let result = unwind(input.trim());
+
+    println!("Result: {}", result);
+
 }
