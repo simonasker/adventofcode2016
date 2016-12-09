9dc0f7fe068855ed867b2683298a9d54355d9757
Simon Andersson <simon@mullvad.net>
2016-12-09 16:22:56 +0100

    Find markers

--- src/solutions/day09.rs
+++ src/solutions/day09.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::str::FromStr;
 
 pub fn run(part: i32) {
     match part {
@@ -11,13 +11,39 @@ pub fn run(part: i32) {
 }
 
 fn part_one() {
-    let f = File::open("input/day09.txt").unwrap();
-    let reader = BufReader::new(f);
+    let mut f = File::open("input/day09.txt").unwrap();
 
-    for line in reader.lines() {
-        let line = line.unwrap();
-        println!("{}", line);
+    let mut input = String::new();
+    let mut output = String::new();
+
+    f.read_to_string(&mut input).unwrap();
+
+    let mut char_iter = input.trim().chars();
+
+    loop {
+        let c = char_iter.next();
+        match c {
+            Some('(') => {
+                let marker: String = char_iter
+                    .by_ref()
+                    .take_while(|&c| c != ')')
+                    .collect();
+                println!("{:?}", marker);
+                let mut asd = marker.split('x');
+                let num_chars = u32::from_str(asd.next().unwrap()).unwrap();
+                let reps = u32::from_str(asd.next().unwrap()).unwrap();
+                println!("Num chars: {}", num_chars);
+                println!("Repetitions: {}", reps);
+            },
+            Some(c) => {
+                output.push(c);
+            },
+            None => break,
+        }
     }
+
+    println!("{}", output);
+
 }
 
 fn part_two() {
