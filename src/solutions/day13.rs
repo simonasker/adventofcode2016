ba774223f9b87688a6e1afd1405f79b2f2fb3455
Simon Andersson <simon@mullvad.net>
2016-12-13 20:54:57 +0100

    WIP: Implement BFS

--- src/solutions/day13.rs
+++ src/solutions/day13.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn run(part: i32) {
     match part {
         1 => part_one(),
@@ -5,6 +7,8 @@ pub fn run(part: i32) {
         _ => println!("Choose part 1 or 2"),
     }
 }
+
+type Node = (u32, u32);
 
 // const INPUT: u32 = 1362;
 const INPUT: u32 = 10;
@@ -16,16 +20,53 @@ fn is_open(x: u32, y: u32) -> bool {
     sum % 2 == 0
 }
 
+fn neighbors(node: Node) -> Vec<Node> {
+    let mut result = Vec::new();
+    let (x, y) = node;
+
+    if x > 0 && is_open(x-1, y) { result.push((x-1, y)); }
+    if is_open(x+1, y) { result.push((x+1, y)); }
+
+    if y > 0 && is_open(x, y-1) { result.push((x, y-1)); }
+    if is_open(x, y+1) { result.push((x, y+1)); }
+
+    result
+}
+
 fn part_one() {
+
+    let mut distances: HashMap<Node, u32> = HashMap::new();
+    let mut parents: HashMap<Node, Option<Node>> = HashMap::new();
+
     for y in 0..40 {
         for x in 0..40 {
-            let sign = match is_open(x, y) {
-                true => '.',
-                false => '#',
-            };
-            print!("{}", sign);
+            if is_open(x, y) {
+                distances.insert((x, y), u32::max_value());
+                parents.insert((x, y), None);
+            }
+
+            // let sign = match is_open(x, y) {
+            //     true => '.',
+            //     false => '#',
+            // };
+            // print!("{}", sign);
         }
-        print!("\n");
+        // print!("\n");
+    }
+
+    let mut q: Vec<Node> = Vec::new();
+
+    let start = (1, 1);
+
+    distances.insert(start, 0);
+    q.insert(0, start);
+
+    while !q.is_empty() {
+        let current = q.pop().unwrap();
+        println!("current: {:?}", current);
+        for n in neighbors(current) {
+            println!("n: {:?}", n);
+        }
     }
 }
 
