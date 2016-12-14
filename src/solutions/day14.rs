d08b99d2994a619225ca7c2ff1ca5899a1d47891
Simon Andersson <simon@mullvad.net>
2016-12-14 21:16:18 +0100

    WIP: Peekable iterator

--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -20,35 +20,51 @@ fn part_one() {
     let mut potential: HashMap<char, Vec<u32>> = HashMap::new();
     let mut found_keys: u32 = 0;
 
-    for n in 0..22729 {
+    'outer: for n in 0.. {
         let mut hasher = Md5::new();
         let foo = format!("{}{}", INPUT, n);
         hasher.input_str(&foo);
         let hash = hasher.result_str();
 
+        // println!("{}: {}", foo, hash);
 
         let mut prev = '-';
         let mut num = 0;
-        for c in hash.chars() {
+        let mut char_iter = hash.chars().peekable();
+
+        while let Some(c) = char_iter.next() {
             if c == prev {
                 num += 1;
             } else {
                 num = 1;
             }
 
-            if num == 3 {
+            let mut next_char = '-';
+            if let Some(nc) = char_iter.peek() {
+                next_char = *nc;
+            }
+
+            if num == 3 && !(next_char == c) {
+                println!("{}: Found triplet", n);
                 let vec = potential.entry(c).or_insert(Vec::new());
                 vec.push(1000);
+                break;
             }
 
             if num == 5 {
+                println!("{}: Found quintuplet", n);
                 let vec = potential.get_mut(&c).expect("There must be a vector for this value");
                 for val in vec.iter_mut() {
                     if *val > 0 {
+                        println!("{}: Found key '{}'", n, c);
                         found_keys += 1;
+                        if found_keys == 64 {
+                            break 'outer;
+                        }
                         *val = 0;
                     }
                 }
+                break;
             }
             prev = c.clone();
         }
