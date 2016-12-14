0aa984279292dcf2faea30541a44304b0b628dd3
Simon Andersson <simon@mullvad.net>
2016-12-14 22:03:37 +0100

    Better definition of triples

--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -17,70 +17,56 @@ pub fn run(part: i32) {
 const INPUT: &'static str = "abc";
 
 fn part_one() {
-    let mut potential: HashMap<char, Vec<u32>> = HashMap::new();
-    let mut found_keys: u32 = 0;
+    let mut char_to_ind: HashMap<char, Vec<u32>> = HashMap::new();
+    let mut ind_to_ctr: HashMap<u32, u32> = HashMap::new();
 
-    'outer: for n in 0.. {
+    'outer: for n in 0..201 {
         let mut hasher = Md5::new();
         let foo = format!("{}{}", INPUT, n);
         hasher.input_str(&foo);
         let hash = hasher.result_str();
+        println!("{}: {}", foo, hash);
 
-        // println!("{}: {}", foo, hash);
-
-        let mut prev = '-';
-        let mut num = 0;
         let mut char_iter = hash.chars().peekable();
 
+        let mut num = 0;
+        let mut prev = '-';
+
         while let Some(c) = char_iter.next() {
-            if c == prev {
-                num += 1;
-            } else {
-                num = 1;
+            if c == prev { num += 1; } else { num = 1; }
+
+            let mut next = '-';
+            if let Some(cn) = char_iter.peek() {
+                next = *cn;
             }
 
-            let mut next_char = '-';
-            if let Some(nc) = char_iter.peek() {
-                next_char = *nc;
-            }
-
-            if num == 3 && !(next_char == c) {
-                println!("{}: Found triplet", n);
-                let vec = potential.entry(c).or_insert(Vec::new());
-                vec.push(1000);
+            if (num == 3 && c != next) || (num == 4 && c != next) {
+                println!("FOUND TRIPLE");
+                char_to_ind.entry(c).or_insert(Vec::new()).push(n);
+                ind_to_ctr.insert(n, 1000);
                 break;
             }
 
             if num == 5 {
-                println!("{}: Found quintuplet", n);
-                let vec = potential.get_mut(&c).expect("There must be a vector for this value");
-                for val in vec.iter_mut() {
-                    if *val > 0 {
-                        println!("{}: Found key '{}'", n, c);
-                        found_keys += 1;
-                        if found_keys == 64 {
-                            break 'outer;
-                        }
-                        *val = 0;
-                    }
-                }
+                println!("FOUND QUINTUPLE");
                 break;
             }
+
+
             prev = c.clone();
         }
 
-        for vec in potential.values_mut() {
-            for val in vec.iter_mut() {
-                if *val > 0 {
-                    *val -= 1;
-                }
+        for val in ind_to_ctr.values_mut() {
+            if *val > 0 {
+                *val -= 1;
             }
         }
     }
 
-    println!("Found keys: {}", found_keys);
-    // println!("{:?}", potential);
+    println!("{:?}", char_to_ind);
+    println!("{:?}", ind_to_ctr);
 }
+
 
 fn part_two() {
     println!("Not yet implemented");
