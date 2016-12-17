c54f00bd4606f47e5a18c5d78c02f430860df5ff
Simon Andersson <simon@mullvad.net>
2016-12-17 08:28:06 +0100

    Day 15 part 1 solved

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -6,44 +6,41 @@ pub fn run(part: i32) {
     }
 }
 
+const NUMBER_OF_DISCS: usize = 7;
+
 fn part_one() {
-    let start_position = [4, 1];
-    let positions = [5, 2];
+    // let start_position = [0, 4, 1];
+    // let positions = [1, 5, 2];
+    let start_position = [0, 1, 0, 2, 0, 0, 5];
+    let positions = [1, 17, 7, 19, 5, 3, 13];
 
     'start_time: for start_time in 0.. {
-        // TODO Remove this
-        // if start_time >= 10 { break; }
 
-        let mut current_positions = [0, 0];
-
-        // Set starting positions
-        for i in 0..start_position.len() {
+        let mut current_positions = [0; NUMBER_OF_DISCS];
+        for i in 0..NUMBER_OF_DISCS {
             current_positions[i] = (start_position[i] + start_time) % positions[i];
         }
 
-        // let mut time = start_time;
         let mut ball = 0;
 
-        println!("START TIME: {}", start_time);
+        println!("\nSTART TIME: {}", start_time);
         loop {
-            // TODO Remove this
-            // time += 1;
-            // if time == start_time + 5 { break; }
 
-            println!("{:?}", current_positions);
+            print!("{:?}", current_positions);
             if current_positions[ball] != 0 {
-                println!("Bounced");
+                println!(" Bounced");
                 continue 'start_time;
             }
+            println!("");
 
             ball += 1;
-            if ball == current_positions.len() {
+            if ball == NUMBER_OF_DISCS {
                 println!("Reached the end when starting at time: {}", start_time);
                 break 'start_time;
             }
 
             // Update discs
-            for i in 0..current_positions.len() {
+            for i in 0..NUMBER_OF_DISCS {
                 current_positions[i] = (current_positions[i] + 1) % positions[i];
             }
         }
