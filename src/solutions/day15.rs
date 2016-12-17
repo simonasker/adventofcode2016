9fd68fb122058d137a13ca1cda8d593a41dee719
Simon Andersson <simon@mullvad.net>
2016-12-17 08:18:06 +0100

    Solved except of by one

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -10,17 +10,42 @@ fn part_one() {
     let start_position = [4, 1];
     let positions = [5, 2];
 
-    for start_time in 0.. {
+    'start_time: for start_time in 0.. {
+        // TODO Remove this
+        // if start_time >= 10 { break; }
+
         let mut current_positions = [0, 0];
 
+        // Set starting positions
         for i in 0..start_position.len() {
             current_positions[i] = (start_position[i] + start_time) % positions[i];
         }
 
-        println!("{:?}", current_positions);
+        // let mut time = start_time;
+        let mut ball = 0;
 
-        if start_time >= 10 {
-            break;
+        println!("START TIME: {}", start_time);
+        loop {
+            // TODO Remove this
+            // time += 1;
+            // if time == start_time + 5 { break; }
+
+            println!("{:?}", current_positions);
+            if current_positions[ball] != 0 {
+                println!("Bounced");
+                continue 'start_time;
+            }
+
+            ball += 1;
+            if ball == current_positions.len() {
+                println!("Reached the end when starting at time: {}", start_time);
+                break 'start_time;
+            }
+
+            // Update discs
+            for i in 0..current_positions.len() {
+                current_positions[i] = (current_positions[i] + 1) % positions[i];
+            }
         }
     }
 }
