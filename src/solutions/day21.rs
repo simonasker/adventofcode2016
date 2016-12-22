ed11523ac68d72c56f5df637b0a3968a2e49da1d
Simon Andersson <simon@mullvad.net>
2016-12-22 08:54:04 +0100

    Create all regexes

--- src/solutions/day21.rs
+++ src/solutions/day21.rs
@@ -19,6 +19,10 @@ fn part_one() {
 
     let re_1 = regex::Regex::new(r"^swap position (\d+) with position (\d+)$").unwrap();
     let re_2 = regex::Regex::new(r"^swap letter (\w) with letter (\w)$").unwrap();
+    let re_3 = regex::Regex::new(r"^reverse positions (\d+) through (\d+)$").unwrap();
+    let re_4 = regex::Regex::new(r"^rotate (\w+) (\d+) step.*$").unwrap();
+    let re_5 = regex::Regex::new(r"^move position (\d+) to position (\d+)$").unwrap();
+    let re_6 = regex::Regex::new(r"^rotate based on position of letter (\w)$").unwrap();
 
     for line in reader.lines() {
         let line = line.unwrap();
@@ -38,6 +42,37 @@ fn part_one() {
             let y = caps.at(2).unwrap();
 
             println!("-> SWAP LETTER: {} {}", x, y);
+        }
+
+        // Reverse positions X through position Y
+        if let Some(caps) = re_3.captures(&line) {
+            let x = i32::from_str(caps.at(1).unwrap()).unwrap();
+            let y = i32::from_str(caps.at(2).unwrap()).unwrap();
+
+            println!("-> REVERSE POSITIONS: {} {}", x, y);
+        }
+
+        // Rotate DIR X steps
+        if let Some(caps) = re_4.captures(&line) {
+            let dir = caps.at(1).unwrap();
+            let x = i32::from_str(caps.at(2).unwrap()).unwrap();
+
+            println!("-> ROTATE: {} {}", dir, x);
+        }
+
+        // Move position X to position Y
+        if let Some(caps) = re_5.captures(&line) {
+            let x = i32::from_str(caps.at(1).unwrap()).unwrap();
+            let y = i32::from_str(caps.at(2).unwrap()).unwrap();
+
+            println!("-> MOVE POSITION: {} {}", x, y);
+        }
+
+        // Rotate based on position of letter X
+        if let Some(caps) = re_6.captures(&line) {
+            let x = caps.at(1).unwrap();
+
+            println!("-> MOVE ON LETTER: {}", x);
         }
     }
 }
